f0a0410bef46464daa98e92a39fea9a2bc3b73b7
Avgeya India <159917433+github-avegya@users.noreply.github.com>
2024-08-01 02:39:22 +0530

    chore: added chromium for vercel

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,6 +1,7 @@
 import puppeteer, { Page } from "puppeteer";
 import { Browser } from "puppeteer";
 import { ByeAnimeDisplayData } from "../types";
+import chromium from '@sparticuz/chromium';
 
 export class ByeAnimeScraper {
     private BASE_URL = 'https://hianime.to';
@@ -14,7 +15,11 @@ export class ByeAnimeScraper {
     // Basic functionalities
 
     static async create(): Promise<ByeAnimeScraper> {
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            headless: true
+        });
         return new ByeAnimeScraper(browser)
     }
 
@@ -28,15 +33,15 @@ export class ByeAnimeScraper {
 
     async display_data(): Promise<ByeAnimeDisplayData[] | null> {
         const page = await this.get_page(this.HOME);
-    
+
         if (!page) {
             return null;
         }
-        
+
         const slides_data: ByeAnimeDisplayData[] = [];
-    
+
         const slides = await page.$$('.swiper-slide:not(.swiper-slide-duplicate)');
-    
+
         for (const slide of slides) {
             const content = await slide.evaluate((element) => {
                 const titleElement = element.querySelector('.deslide-item > .deslide-item-content > .desi-head-title.dynamic-name');
@@ -44,13 +49,13 @@ export class ByeAnimeScraper {
                 const descriptionElement = element.querySelector('.desi-description');
                 const watchButtonElement = element.querySelector('.desi-buttons > .btn-secondary') as HTMLAnchorElement;
                 const bannerElement = element.querySelector('.deslide-cover > .deslide-cover-img > img');
-    
+
                 const title = titleElement ? titleElement.textContent : '';
                 const ranking = rankingElement ? rankingElement.textContent : '';
                 const description = descriptionElement ? descriptionElement.textContent?.replace(/\n/g, '').trim() : '';
                 const slug = watchButtonElement ? watchButtonElement.getAttribute('href')?.replace("https://hianime.to/watch/", "").replace("/", "") : '';
                 const banner = bannerElement ? bannerElement.getAttribute('data-src') : '';
-    
+
                 const types: string[] = [];
                 element.querySelectorAll('.deslide-item > .deslide-item-content > .sc-detail > .scd-item').forEach((detail) => {
                     const text = detail?.textContent;
@@ -58,7 +63,7 @@ export class ByeAnimeScraper {
                         types.push(text.replace(/\n/g, '').trim());
                     }
                 });
-    
+
                 return {
                     title,
                     ranking,
@@ -68,17 +73,17 @@ export class ByeAnimeScraper {
                     banner
                 };
             });
-    
+
             if (content && content.title) { // Ensure that we have a valid title before pushing to slides_data
                 slides_data.push(content);
             }
         }
-    
+
         this.close();
-    
+
         return slides_data;
     }
-    
+
 
     // Helper Functionalities
 
